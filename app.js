8296195159cd9db1a39be4809f0b696ba14bfd53
Jarren San Jose <sanjose.jarren@gmail.com>
2018-09-23 18:03:42 -0700

    koa-router and koa-ejs

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
 const json = require('koa-json');
+const path = require('path');
+const render = require('koa-ejs');
 
 const app = new Koa();
+const router = new KoaRouter();
 
+// json prettier middleware
 app.use(json());
 
-app.use(async ctx => ctx.body = {msg: 'hi'});
+// simple middleware example
+// app.use(async ctx => ctx.body = {msg: 'hi'});
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+// index
+router.get('/', async ctx => {
+  await ctx.render('index');
+});
+
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+// router middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server started!'))
